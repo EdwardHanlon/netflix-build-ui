56fcaf317780f69f41e9d56a8f0cf79609717ab4
Edward Hanlon <EJHanlon03@gmail.com>
2023-04-01 14:49:28 -0400

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyAd-PnxvP_rwcP7OJJSisR2hsljJyrA_94",
+	authDomain: "netflix-build-ui.firebaseapp.com",
+	projectId: "netflix-build-ui",
+	storageBucket: "netflix-build-ui.appspot.com",
+	messagingSenderId: "610281702843",
+	appId: "1:610281702843:web:d218f10501e06a882e309e"
+  };
