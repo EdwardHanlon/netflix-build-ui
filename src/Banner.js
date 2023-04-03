561d59a728f3d643b7a84d62e836fff34cc84d6c
Edward Hanlon <EJHanlon03@gmail.com>
2023-04-03 16:31:07 -0400

    built out HomeScreen, Row and Banner components

--- src/Banner.js
+++ src/Banner.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import "./Banner.css";
+import axios from "./axiosClient";
+import requests from './Requests'
 
 function Banner() {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(()=> {
+	async function fetchData() {	
+		const request = await axios.get(requests.fetchNetflixOriginals);		
+		
+		setMovie(request.data.results[
+			Math.floor(Math.random() * request.data.results.length - 1)
+		]);
+
+		return request;
+	}
+
+	fetchData();	
+  }, [])
+ 
   function truncate(string, n) {
     return string?.length > n ? string.substring(0, n - 1) + "..." : string;
   }
@@ -10,48 +28,21 @@ function Banner() {
   return (
     <header
       className="banner"
-      // style={{
-      // 	backgroundSize: "cover",
-      // 	backgroundImage: `url('https://raw.githubusercontent.com/thatanjan/netflix-clone-yt/youtube/media//banner.jpg')`,
-      // 	backgroundPosition: "center center"
-      // }}>
-
       style={{
         backgroundSize: "cover",
-        backgroundImage: `url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAASIAAACuCAMAAAClZfCTAAAAA1BMVEUAAACnej3aAAAASElEQVR4nO3BMQEAAADCoPVPbQhfoAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABeA8XKAAFZcBBuAAAAAElFTkSuQmCC')`,
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
         backgroundPosition: "center center",
       }}
     >
       <div className="banner__contents">
-        <h1 className="banner__title">Movie Name</h1>
+        <h1 className="banner__title">{movie?.title || movie?.name || movie?.original_name}</h1>
         <div className="banner__buttons">
           <button className="banner__button">Play</button>
           <button className="banner__button">My List</button>
         </div>
         <h1 className="banner__description">
           {truncate(
-            `This is a test descriptionThis is a test descriptionThis is a test descriptionThis is 
-			a test descriptionThis is a test descriptionThis is a test descriptionThis is a test 
-			descriptionThis is a test descriptionThis is a test descriptionThis is a test 
-			descriptionThis is a test descriptionThis is a test descriptionThis is a test 
-			descriptionThis is a test descriptionThis is a test descriptionThis is a test 
-			descriptionThis is a test descriptionThis is a test descriptionThis is a test 
-			descriptionThis is a test descriptionThis is a test descriptionThis is a test
-			 descriptionThis is a test descriptionThis is a test descriptionThis is a test 
-			 descriptionThis is a test descriptionThis is a test descriptionThis is a test
-			  descriptionThis is a test descriptionThis is a test descriptionThis is a test 
-			  descriptionThis is a test descriptionThis is a test descriptionThis is a test
-			   descriptionThis is a test descriptionThis is a test descriptionThis is a test
-			   descriptionThis is a test descriptionThis is a test descriptionThis is a test 
-			   descriptionThis is a test descriptionThis is a test descriptionThis is a test
-			    descriptionThis is a test descriptionThis is a test descriptionThis is a test 
-				descriptionThis is a test descriptionThis is a test descriptionThis is a test 
-				descriptionThis is a test descriptionThis is a test descriptionThis is a test 
-				descriptionThis is a test descriptionThis is a test descriptionThis is a test
-				 descriptionThis is a test descriptionThis is a test descriptionThis is a test 
-				 descriptionThis is a test descriptionThis is a test descriptionThis is a test 
-				 descriptionThis is a test descriptionThis is a test descriptionThis is a test 
-				 description`,
+            movie?.overview,
             150
           )}
         </h1>
